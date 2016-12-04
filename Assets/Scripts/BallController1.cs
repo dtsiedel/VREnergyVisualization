e7f1daec71c614ac8f81d54b1988986fba346967
Drew Siedel <dts.siedel@gmail.com>
2016-12-04 09:51:24 -0500

    Final Version from Fall LehighHacks 2016

---
+++ Assets/Scripts/BallController1.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+
+public class BallController1 : MonoBehaviour {
+
+	Dictionary<int, string> buildings;
+	GameObject canvas;
+	string serverData = "RauchChiller";
+	string usageData = "NA";
+
+	// Use this for initialization
+	void Start () 
+	{
+		canvas = GameObject.Find("Canvas");
+		StartCoroutine (GetText ());
+		StartCoroutine (GetUsagePerSquareFoot ());
+	}
+		
+
+	// Update is called once per frame
+	void Update ()
+	{
+		string thisBuilding = "RauchChiller";
+
+		Text[] textValue = canvas.GetComponentsInChildren<Text> ();
+		textValue [0].text = "Building: " + thisBuilding + relevantData(thisBuilding) + relevantUsage(thisBuilding);
+
+
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene("Scene2");
+		}
+			
+	}
+
+	//returns the data from the JSON string in serverData,
+	//but only with the data we actually care about
+	string relevantData(string thisBuilding)
+	{
+		JSONObject serverJSON = new JSONObject (serverData);
+		JSONObject buildingRelevant = serverJSON[thisBuilding];
+
+		JSONObject DPU = buildingRelevant ["DPU"];
+		JSONObject DEU = buildingRelevant ["DEU"];
+		JSONObject WU = buildingRelevant ["WU"];
+
+		return "\nDPU: " + DPU.ToString() + " kWH\nDEU: " + DEU.ToString() + " kW\nWU: " + WU.ToString() + " W";
+	
+	}
+
+	//extracts the data from the JSON string *usageData* into
+	//three strings, to then be put on the end of our data
+	string relevantUsage(string thisBuilding)
+	{
+		JSONObject usageJSON = new JSONObject (usageData);
+		JSONObject buildingRelevant = usageJSON [thisBuilding];
+
+		JSONObject DPU = buildingRelevant ["DPU"];
+		JSONObject DEU = buildingRelevant ["DEU"];
+		JSONObject WU = buildingRelevant ["WU"];
+
+		return "\nDPU/sq ft: " + DPU.ToString() + " \nDEU/sq ft: " + DEU.ToString() + " \nWU/sq ft: " + WU.ToString() + " ";
+	}
+
+	//the function that gets text from the server
+	IEnumerator GetText() {
+		string server = "http://ec2-54-186-143-244.us-west-2.compute.amazonaws.com/json/power_data_current.json";
+		using(UnityWebRequest www = UnityWebRequest.Get(server)) {
+			yield return www.Send();
+
+
+			if(www.isError) {
+				Application.Quit ();
+			}
+			else {
+				serverData = www.downloadHandler.text;
+			}
+
+		}
+	}
+		
+	//gets the average data from the server,
+	//and also returns that value so that it can be
+	//put into the summary
+	IEnumerator GetUsagePerSquareFoot()
+	{
+		string server = "http://ec2-54-186-143-244.us-west-2.compute.amazonaws.com/file/per-foot-squared.json";
+		using(UnityWebRequest www = UnityWebRequest.Get(server)) {
+			yield return www.Send();
+
+			if(www.isError) {
+				Application.Quit ();
+			}
+			else {
+				usageData = www.downloadHandler.text;
+			}
+
+		}
+	}
+
+}
